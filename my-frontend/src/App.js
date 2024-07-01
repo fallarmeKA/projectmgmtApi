73ba965095b484fdab94b83c7f89438c7474c85a
KYLE FALLARME <kalfallarme@mymail.mapua.edu.ph>
2024-07-01 16:30:41 +0800

    frontend

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProjectPage from './pages/ProjectPage';
+import TaskPage from './pages/TaskPage';
+// Import other pages as needed
 
-function App() {
+const App = () => {
   return (
-    <>
-    Hello
-    </>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/projects" component={ProjectPage} />
+        <Route path="/tasks" component={TaskPage} />
+        {/* Add other routes as needed */}
+      </Switch>
+    </div>
   );
 }
 
